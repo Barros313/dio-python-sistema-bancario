8882b443ba291ae40fdacb5decd64db7c05c9770
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-09-02 10:25:22 -0300

    Added client class
    Signed-off-by: Gabriel Barros de Almeida <gbarros1337@gmail.com>

---
+++ Client.py
@@ -0,0 +1,10 @@
+class Client:
+    def __init__(self, address, accounts):
+        self._address = address
+        self._accounts = accounts
+
+    def make_transaction(account, transaction):
+        pass
+
+    def add_account(account):
+        pass
